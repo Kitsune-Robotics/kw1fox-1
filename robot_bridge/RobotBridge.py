62d1c9a5c362f380d5f2bc41a3d64f967710aba7
KenwoodFox <kenwood364@gmail.com>
2022-04-20 13:51:57 -0400

    Boilerplate robot_bridge

---
+++ robot_bridge/RobotBridge.py
@@ -0,0 +1,16 @@
+# Kitsune Scientific
+
+
+class RobotBridge:
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        while True:
+            pass
+
+
+if __name__ == "__main__":
+    robotBridge = RobotBridge()
+
+    robotBridge.run()
