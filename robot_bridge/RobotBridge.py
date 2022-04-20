3b49ebcc6e54975462a41b135c7ab585924b913e
KenwoodFox <kenwood364@gmail.com>
2022-04-20 14:04:43 -0400

    iron out sim mode

--- robot_bridge/RobotBridge.py
+++ robot_bridge/RobotBridge.py
@@ -1,9 +1,15 @@
 # Kitsune Scientific
 
+import argparse
+import logging
+
 
 class RobotBridge:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, _args) -> None:
+        # Copy in args
+        self.args = _args
+
+        logging.info(f"Starting RobotBridge, sim mode = {self.args.sim}")
 
     def run(self):
         while True:
@@ -11,6 +17,18 @@ class RobotBridge:
 
 
 if __name__ == "__main__":
-    robotBridge = RobotBridge()
+    # Setup logging
+    logging.basicConfig(level=logging.DEBUG)
 
+    # Argument parser
+    parser = argparse.ArgumentParser(description="Robot Bridge Args.")
+    parser.add_argument("--sim", default=False, action="store_true")
+
+    # Parse args
+    args = parser.parse_args()
+
+    # Make a bridge
+    robotBridge = RobotBridge(args)
+
+    # Run the bridge
     robotBridge.run()
