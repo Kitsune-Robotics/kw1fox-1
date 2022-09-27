7a544658e9a837e74ebe7cf5aa25377d7c54d91c
KenwoodFox <kenwood364@gmail.com>
2022-09-27 13:05:02 -0400

    Create TNC image

---
+++ Houston-Direwolf/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start audio
+export "PULSE_SERVER=unix:/tmp/pulsesocket" && pulseaudio --system &
+
+# Wait for any process to exit
+wait -n
+  
+# Exit with status of process that exited first
+exit $?
