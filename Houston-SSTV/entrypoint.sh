bc6e2a76e9a8a26ed2698a962fb944159be48ac0
KenwoodFox <kenwood364@gmail.com>
2022-10-17 11:28:19 -0400

    We can pause here

--- Houston-SSTV/entrypoint.sh
+++ Houston-SSTV/entrypoint.sh
@@ -4,13 +4,16 @@
 Xvfb :60 -screen 0 1920x1080x16 &
 
 # Start audio
-export "PULSE_SERVER=unix:/tmp/pulsesocket" && pulseaudio --system &
+# export "PULSE_SERVER=unix:/tmp/pulsesocket" && pulseaudio --system &
 
 # Start qsstv
 export DISPLAY=:60 && qsstv &
 
+# Start videostreamer
+export DISPLAY=:60 && ./streamer_binary &
+
 # For testing
-x11vnc -passwd iamnotacrook -display :60 -N -forever &
+# x11vnc -passwd iamnotacrook -display :60 -N -forever &
 
 # Wait for any process to exit
 wait -n
