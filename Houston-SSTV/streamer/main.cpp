35f6b50125d4e4b1ebd954aecd4eb3795fdaae6e
KenwoodFox <kenwood364@gmail.com>
2022-10-17 11:00:09 -0400

    This works even better

--- Houston-SSTV/streamer/main.cpp
+++ Houston-SSTV/streamer/main.cpp
@@ -1,19 +1,39 @@
 #include <opencv2/opencv.hpp>
 
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <cstdint>
+#include <cstring>
+#include <vector>
+
 #include <nadjieb/mjpeg_streamer.hpp>
 
 // for convenience
 using MJPEGStreamer = nadjieb::MJPEGStreamer;
 
+void ImageFromDisplay(std::vector<uint8_t> &Pixels, int &Width, int &Height, int &BitsPerPixel)
+{
+    Display *display = XOpenDisplay(nullptr);
+    Window root = DefaultRootWindow(display);
+
+    XWindowAttributes attributes = {0};
+    XGetWindowAttributes(display, root, &attributes);
+
+    Width = attributes.width;
+    Height = attributes.height;
+
+    XImage *img = XGetImage(display, root, 0, 0, Width, Height, AllPlanes, ZPixmap);
+    BitsPerPixel = img->bits_per_pixel;
+    Pixels.resize(Width * Height * 4);
+
+    memcpy(&Pixels[0], img->data, Pixels.size());
+
+    XDestroyImage(img);
+    XCloseDisplay(display);
+}
+
 int main()
 {
-    cv::VideoCapture cap(0);
-    if (!cap.isOpened())
-    {
-        std::cerr << "VideoCapture not opened\n";
-        exit(EXIT_FAILURE);
-    }
-
     std::vector<int> params = {cv::IMWRITE_JPEG_QUALITY, 90};
 
     MJPEGStreamer streamer;
@@ -27,11 +47,18 @@ int main()
     //      streamer.start(8080, 4);
     streamer.start(8080);
 
+    int Width = 0;
+    int Height = 0;
+    int Bpp = 0;
+    std::vector<std::uint8_t> Pixels;
+
     // Visit /shutdown or another defined target to stop the loop and graceful shutdown
     while (streamer.isRunning())
     {
-        cv::Mat frame;
-        cap >> frame;
+        ImageFromDisplay(Pixels, Width, Height, Bpp);
+
+        cv::Mat frame = cv::Mat(Height, Width, Bpp > 24 ? CV_8UC4 : CV_8UC3, &Pixels[0]);
+
         if (frame.empty())
         {
             std::cerr << "frame not grabbed\n";
