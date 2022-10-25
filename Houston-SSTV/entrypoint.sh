6843b2ca6201c3ba8ac3451c898a6dccf3556d4f
KenwoodFox <kenwood364@gmail.com>
2022-10-25 14:52:02 -0400

    Maybe this will help, who can say
    Attempted fix for #9

--- Houston-SSTV/entrypoint.sh
+++ Houston-SSTV/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start xfvb
-Xvfb :60 -screen 0 1920x1080x16 &
+Xvfb :60 -screen 0 1920x1080x32 &
 
 # Start audio
 # export "PULSE_SERVER=unix:/tmp/pulsesocket" && pulseaudio --system &
@@ -10,7 +10,7 @@ Xvfb :60 -screen 0 1920x1080x16 &
 export DISPLAY=:60 && qsstv &
 
 # Start videostreamer
-# export DISPLAY=:60 && ./streamer_binary &
+./streamer_binary &
 
 # For testing
 # x11vnc -passwd iamnotacrook -display :60 -N -forever &
