a3276e97c3d78b254093ba3837108082178d663c
KenwoodFox <kenwood364@gmail.com>
2023-02-28 15:49:05 -0500

    Cheat and use turboflask

--- MissionController/app/app.py
+++ MissionController/app/app.py
@@ -1,28 +1,45 @@
-# CS-114
-
-# External libs
-import os
-
-from flask import Flask, render_template, jsonify
-
-
-# Our own
-from tools.misc import get_uptime
+import random
+import re
+import sys
+import threading
+import time
+from flask import Flask, render_template
+from turbo_flask import Turbo
 
 app = Flask(__name__)
+turbo = Turbo(app)
+
+
+@app.context_processor
+def inject_load():
+    if sys.platform.startswith("linux"):
+        with open("/proc/loadavg", "rt") as f:
+            load = f.read().split()[0:3]
+    else:
+        load = [int(random.random() * 100) / 100 for _ in range(3)]
+    return {"load1": load[0], "load5": load[1], "load15": load[2]}
 
 
 @app.route("/")
 def index():
-    try:
-        return render_template("index.html", uptime=get_uptime())
-    except:
-        return render_template("error.html")
+    return render_template("index.html")
 
 
-@app.route("/cmds/version", methods=["GET"])
-def version():
-    return jsonify(version=os.getenv("GIT_COMMIT"))
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")
+
+
+@app.before_first_request
+def before_first_request():
+    threading.Thread(target=update_load).start()
+
+
+def update_load():
+    with app.app_context():
+        while True:
+            time.sleep(5)
+            turbo.push(turbo.replace(render_template("loadavg.html"), "load"))
 
 
 if __name__ == "__main__":
